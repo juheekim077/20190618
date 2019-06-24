cbdedc9f70bffd4272fad0f23cc81850bf1f7bd8
bitcamp <bitcamp@bit>
2019-06-24 16:19:23 +0900

    2019-6-24 게시판

---
+++ sp2/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,41 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/board/*")
+@Log4j
+@AllArgsConstructor
+public class BoardController {
+
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void listAll(Model model) {
+			
+		model.addAttribute("list",service.listAll());
+	}
+	
+	@GetMapping("/register")
+	public void register() {		
+		log.info("get register.....................");
+	}
+	@PostMapping("/register")
+	public String register(BoardVO vo,RedirectAttributes rttr) {
+		
+		log.info("vo: "+vo);
+		service.register(vo);
+		rttr.addFlashAttribute("result","success");
+		return "redirect:/board/list";
+	}
+}
